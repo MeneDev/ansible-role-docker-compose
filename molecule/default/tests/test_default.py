446513c594b251870813640cb8fee54aec0f9ef3
Mene <arndt@menedev.de>
2017-12-07 18:25:34 +0100

    :construction: change vars format

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -9,4 +9,4 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 def test_hosts_file(host):
     version_stdout = host.check_output('docker-compose version')
     assert "docker-compose version %s, build" % (
-        os.environ['DOCKER_COMPOSE_VERSION']) in version_stdout
+        os.environ['DOCKER_COMPOSE_VERSION_EXPECT']) in version_stdout
